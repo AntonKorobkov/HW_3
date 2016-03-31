a2c4023f2571347a3c215ad25285bed65ed6fdf8
Anton Korobkov <korobgit@gmail.com>
2016-03-31 18:46:36 +0300

    just some piece of lab

--- homework_3_Korobkov.py
+++ homework_3_Korobkov.py
@@ -1,46 +1,65 @@
 # -*- coding: utf-8 -*-
 
-from pybrain.structure import FeedForwardNetwork, LinearLayer, TanhLayer, FullConnection
-from pybrain.datasets.supervised import SupervisedDataSet
 import numpy as np
 import matplotlib.dates as md
+from sklearn.neural_network import MLPRegressor
+from parametres import input_sizes, layer_sizes
 
 __author__ = 'Anton Korobkov'
 
-# 1. Given the time series (gold.dlm) of prices for gold.
-# 2. Construct the neural network for forecasting
-#     2.1. Define the length of "input window" (the number of input values for neural network)
-#     2.2. Try different numbers of neurons in hidden layer
-#     2.3. Perform the forecasting
-#            2.3.1. Define the efficient forecasting horyzon (number of forecasted values with an appropriate error)
-# 3. Make conclusions on the results of forecasting using different lengths of "input window" and numbers of neurons in hidden layer
-#
-# Deadline: the end of 3rd module
-
-
 # Load the data and save it into appropriate structure, we also need to figure out how much rows we have
 observations = np.loadtxt('gold.dlm',  converters={0: md.datestr2num, 1: float}, skiprows=1)
-train_length = observations.shape[0]/2
 
-# Create 'train' and 'test' sets
-train, test = observations[0:train_length], observations[train_length:]
+# transform date
+for obsnum, obs in enumerate(observations):
+    observations[obsnum][0] = obsnum
 
-ds = SupervisedDataSet(2, 2)
 
-for rownum in range(0, train_length):
-    ds.addSample(train[rownum][0], train[rownum][1])
+def construct_train(train_length, **kwargs):
+    """
+    Train and test model with given input
+    window and number of neurons in layer
+    """
 
-n = FeedForwardNetwork()
-inLayer = LinearLayer(2)
-hiddenLayer = TanhLayer(25)
-outLayer = LinearLayer(1)
-n.addInputModule(inLayer)
-n.addModule(hiddenLayer)
-n.addOutputModule(outLayer)
+    # set variables to constants
+    start_cur_postion = 0
+    train = observations[start_cur_postion:train_length]
+    steps, steplen = observations.size/(2 * train_length), train_length
 
-in_to_hidden = FullConnection(inLayer, hiddenLayer)
-hidden_to_out = FullConnection(hiddenLayer, outLayer)
-n.addConnection(in_to_hidden)
-n.addConnection(hidden_to_out)
-n.sortModules()
+    if 'hidden_layer' in kwargs:
+        network = MLPRegressor(hidden_layer_sizes=kwargs['hidden_layer'])
+    else:
+        network = MLPRegressor()
 
+    quality = []
+
+    # fit model - configure parameters
+    network.fit(train[:, 1].reshape(1, len(train)), train[:, 1].reshape(1, len(train)))
+
+    parts = []
+
+    # calculate predicted values
+    for i in xrange(0, steps):
+        # print start_cur_postion, train_length
+        parts.append(network.predict(observations[start_cur_postion:train_length][:, 1]))
+        start_cur_postion += steplen
+        train_length += steplen
+
+    # estimate model quality
+    result = np.array(parts).flatten().tolist()
+    for valnum, value in enumerate(result):
+        quality.append((value - observations[valnum][1])**2)
+
+    return sum(quality)/len(quality)
+
+
+def main():
+    result_list = open('analysis_results.txt', 'w')
+
+    for size in input_sizes:
+        for layer in layer_sizes:
+            result_list.write(' '.join(['Input window:', str(size), 'Neurons:', str(layer), 'Resulting error:',
+                                        str(construct_train(size, hidden_layer=layer)), '\n']))
+
+if __name__ == "__main__":
+    main()
---
+++ parametres.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Parameters to train and test model
+"""
+
+__author__ = 'Anton Korobkov'
+
+input_sizes = [10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100, 200, 500, 600, 700, 800, 900, 1000]
+layer_sizes = [(50,), (25,), (20,), (150,), (200,)]
+
